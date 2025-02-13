81e164501ae2a5d419d4b2d6e76675a8b4d8a85d
Roman <myakunovre@yandex.ru>
2025-02-13 22:01:49 +0300

    Исправлено по тестам github

--- src/main/java/ru/practicum/shareit/item/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/ItemService.java
@@ -5,8 +5,12 @@ import java.util.Optional;
 
 interface ItemService {
     ItemDto addNewItem(long userId, Item item);
+
     ItemDto updateItem(Long userId, Long itemId, Item item);
+
     Optional<ItemDto> getItem(Long itemId);
+
     List<ItemDto> getItems(long userId);
+
     List<ItemDto> getSearch(String text);
 }
