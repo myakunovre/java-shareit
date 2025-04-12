4f1a1498a3883cc68f2434ca9bffd67ebb9e79e2
Roman <myakunovre@yandex.ru>
2025-04-12 08:10:11 +0300

    Исправлены замечания checkstyle github

--- src/main/java/ru/practicum/shareit/ShareItApp.java
+++ src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -3,11 +3,9 @@ package ru.practicum.shareit;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @Slf4j
-@ComponentScan
 public class ShareItApp {
 
 	public static void main(String[] args) {
