88fb0e6c08e987d5d45fbb5774226311794b5943
Roman <myakunovre@yandex.ru>
2025-06-15 18:51:24 +0300

    Доработано по финальному ТЗ (16 Спринт) + замечания github

--- server/src/test/java/ru/practicum/shareit/user/UserServiceImplTest.java
+++ server/src/test/java/ru/practicum/shareit/user/UserServiceImplTest.java
@@ -12,13 +12,12 @@ import ru.practicum.shareit.ShareItServer;
 
 import java.util.List;
 
-import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.equalTo;
 
 @Transactional
 @SpringBootTest(
-//        properties = "jdbc.url=jdbc:postgresql://localhost:5432/test",
         webEnvironment = SpringBootTest.WebEnvironment.NONE,
         classes = ShareItServer.class)
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
@@ -26,24 +25,6 @@ class UserServiceImplTest {
 
     private final EntityManager em;
     private final UserService service;
-
-//    @Test
-//    void testSaveUser() {
-//        UserDto userDto = makeUserDto("some@email.com", "Пётр", "Иванов");
-//
-//        service.saveUser(userDto);
-//
-//        TypedQuery<User> query = em.createQuery("Select u from User u where u.email = :email", User.class);
-//        User user = query.setParameter("email", userDto.getEmail())
-//                .getSingleResult();
-//
-//        assertThat(user.getId(), notNullValue());
-//        assertThat(user.getFirstName(), equalTo(userDto.getFirstName()));
-//        assertThat(user.getLastName(), equalTo(userDto.getLastName()));
-//        assertThat(user.getEmail(), equalTo(userDto.getEmail()));
-//        assertThat(user.getState(), equalTo(userDto.getState()));
-//        assertThat(user.getRegistrationDate(), notNullValue());
-//    }
 
     @Test
     void getAllUsers() {
@@ -55,12 +36,7 @@ class UserServiceImplTest {
 
         TypedQuery<User> query = em.createQuery("Select u from User u", User.class);
         List<User> users = query.getResultList();
-//        List<UserDto> userDtos = users.stream()
-//                .map(UserMapper::toUserDto)
-//                .toList();
 
-//        assertThat(userDtos, hasItem(userDto1));
-//        assertThat(userDtos, allOf(hasItem(userDto1), hasItem(userDto2)));
         assertThat(users.size(), equalTo(2));
 
         assertThat(users, containsInAnyOrder(
